39eabfa50b76dec37592d51ddf3888b1cae3f42d
joannawolthuis <jcwolthuis@gmail.com>
2017-09-05 20:32:02 +0200

    adjusted sh script

--- backend/scripts/make_db.sh
+++ backend/scripts/make_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# -l h_rt=07:00:00
-# -l h_vmem=40G
-# -pe threaded 40
-# -m beas
-# -M j.c.wolthuis-2@umcutrecht.nl
+#$ -N makedb
+#$ -l h_rt=07:00:00
+#$ -l h_vmem=40G
+#$ -pe threaded 40
+#$ -m beas
+#$ -M j.c.wolthuis-2@umcutrecht.nl
 
 /home/cog/jwolthuis/R-3.4.0/bin/Rscript --no-save /hpc/cog_bioinf/ridder/users/jwolthuis/MetaboShiny/backend/scripts/db.make.hpc.R
