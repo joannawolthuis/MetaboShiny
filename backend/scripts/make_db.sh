25e286b059463e8fbc536d561b4d89f9cb412b56
joannawolthuis <jcwolthuis93@gmail.com>
2017-09-09 02:51:25 +0200

    Continue mode gives segfault on HPC, maybe something with tmpdir? default to new mode now.

--- backend/scripts/make_db.sh
+++ backend/scripts/make_db.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#$ -N makedb
+#$ -N makedb_cont
 #$ -l h_rt=72:00:00
-#$ -l h_vmem=50G
+#$ -l h_vmem=40G
 #$ -pe threaded 39
 #$ -m beas
 #$ -M j.c.wolthuis-2@umcutrecht.nl
+#$ -m beas
 
-TMPDIR=/hpc/cog_bioinf/ridder/users/jwolthuis/MetaboShiny/backend/db/tmp /home/cog/jwolthuis/R-3.4.0/bin/Rscript --no-save /hpc/cog_bioinf/ridder/users/jwolthuis/MetaboShiny/backend/scripts/db.make.hpc.R
+TMPDIR=/hpc/cog_bioinf/ridder/users/jwolthuis/MetaboShiny/backend/tmp /home/cog/jwolthuis/R-3.4.0/bin/Rscript --no-save /hpc/cog_bioinf/ridder/users/jwolthuis/MetaboShiny/backend/scripts/db.make.hpc.R
